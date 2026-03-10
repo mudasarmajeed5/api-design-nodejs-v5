87ac0d88edc43818d7e222aebf3b6ba1898f8bac
mudasarmajeed5 <mudasarmajeed5@yahoo.com>
2026-03-10 17:29:02 +0500

    63%

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -1,10 +1,11 @@
 import type { Request, Response } from 'express'
-import bcrypt from 'bcrypt'
+import bcrypt from 'bcryptjs'
 import { db } from '../db/connection.ts'
 import { users, type NewUser } from '../db/schema.ts'
 
 import { generateToken } from '../utils/jwt.ts'
-import { hashPassword } from '../utils/passwords.ts'
+import { comparePasswords, hashPassword } from '../utils/passwords.ts'
+import { eq } from 'drizzle-orm'
 
 export const register = async (
   req: Request<any, any, NewUser>,
@@ -38,8 +39,48 @@ export const register = async (
       token,
     })
   } catch (e) {
-    console.error('Registeration error', e)
     // at this point problem is on our side, so we send 500.
-    res.status(500).json({ error: 'Failed to create user.' })
+    return res.status(500).json({ error: 'Failed to create user.' })
+  }
+}
+
+export const login = async (req: Request, res: Response) => {
+  try {
+    const { email, password } = req.body
+    // if any error our responsibility
+    const user = await db.query.users.findFirst({
+      where: eq(users.email, email),
+    })
+    // if we do if(user) then we would have to nest the conditions, so it better to do if(!user)
+    if (!user) {
+      return res.status(401).json({ error: 'Invalid credentials.' })
+    }
+    // if any error our responsibility
+    const isValidPassword = await comparePasswords(password, user.password)
+    if (!isValidPassword) {
+      return res.status(401).json({ error: 'Invalid credentials.' })
+    }
+
+    // if any error our responsibility
+    const token = await generateToken({
+      email: user.email,
+      id: user.id,
+      username: user.username,
+    })
+    return res
+      .json({
+        message: 'Login successful',
+        user: {
+          id: user.id,
+          email: user.email,
+          username: user.username,
+          firstName: user.firstName,
+          lastName: user.lastName,
+        },
+        token,
+      })
+      .status(201)
+  } catch (error) {
+    return res.status(500).json({ error: 'Failed to login' })
   }
 }
--- src/routes/authRoutes.ts
+++ src/routes/authRoutes.ts
@@ -1,11 +1,16 @@
 import { Router } from 'express'
+import { login, register } from '../controllers/authController.ts'
+import { validateBody } from '../middleware/validation.ts'
+import { insertUserSchema } from '../db/schema.ts'
+import z from 'zod'
+const loginSchema = z.object({
+  email: z.email(),
+  password: z.string().min(1, 'Password is required.'),
+})
 const router = Router()
 
 // 201 means your POST request was good
-router.post('/register', (req, res) => {
-  res.status(201).json({ message: 'User signed up' })
-})
-router.post('/login', (req, res) => {
-  res.status(201).json({ message: 'User logged in' })
-})
+router.post('/register', validateBody(insertUserSchema), register)
+
+router.post('/login', validateBody(loginSchema), login)
 export default router
